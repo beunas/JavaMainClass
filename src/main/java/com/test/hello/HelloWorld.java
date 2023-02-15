e6b192dd8473a538debc79738ab8a400ac655687
beunas12@gmail.com <Noonu@1991>
2023-02-15 20:05:05 +0530

    initilacommit

---
+++ src/main/java/com/test/hello/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.test.hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!!!!");
+        System.out.println("Hooks!");
+        System.out.println("Hooks2!");
+    }
+}
